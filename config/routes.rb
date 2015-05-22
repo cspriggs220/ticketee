604c3be4b56f4629cc1fa723659d6fc390453939
Cody Spriggs <codyspriggs@me.com>
2015-05-21 23:54:06 -0500

    Add admin namespace with application controller and catchup CSS to updated book

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root 'application#index'
+  end
+
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -6,7 +11,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'projects#index'
 
-  resources :projects do 
+  resources :projects do
     resources :tickets
   end
 
