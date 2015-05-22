17d7c3bd990a14bf3be77b6500f3d9adc9d6d335
Cody Spriggs <codyspriggs@me.com>
2015-05-22 00:28:20 -0500

    Only admins can create or delete projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,32 +1,14 @@
 class ProjectsController < ApplicationController
-	before_action :set_project, only: [:show, :edit, :update, :destroy]
+	before_action :set_project, only: [:show, :edit, :update]
 
 	def index
 		@projects = Project.all
 	end
 
-	def new
-		@project = Project.new
-	end
-
-	def create
-	  @project = Project.new(project_params)
-		  
-		  if @project.save
-		    flash[:notice] = "Project has been created."
-		    redirect_to @project
-		  else
-		    flash.now[:alert] = 'Project has not been created.'
-		    render 'new'
-		end
-	end
-
 	def show
-		@project = Project.find(params[:id])
 	end
 
 	def edit
-		@project = Project.find(params[:id])
 	end
 
 	def update
@@ -39,23 +21,14 @@ class ProjectsController < ApplicationController
 		end
 	end
 
-	def destroy
-		@project.destroy
-
-		flash[:notice] = "Project has been deleted."
-
-		redirect_to projects_path
-	end
-
 	private
 
 	def set_project
 		@project = Project.find(params[:id])
-		rescue ActiveRecord::RecordNotFound
-			flash[:alert] = "The project you were looking" +
-											" for could not be found."
-
-			redirect_to projects_path
+	rescue ActiveRecord::RecordNotFound
+		flash[:alert] = "The project you were looking" +
+										" for could not be found."
+		redirect_to projects_path
 	end
 
 	def project_params
--- spec/features/admin/creating_projects_spec.rb
+++ spec/features/admin/creating_projects_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper"
 
 RSpec.feature "Creating Projects" do
-	before do 
+  before do
     login_as(FactoryGirl.create(:user, :admin))
-		visit '/'
+    visit '/'
 
-		click_link 'New Project'
-	end
+    click_link 'New Project'
+  end
 
   scenario "A user can create a new project" do
     fill_in "Name", with: "Sublime Text 3"
@@ -22,10 +22,10 @@ RSpec.feature "Creating Projects" do
     expect(page).to have_title(title)
   end
 
-  scenario 'A user cannot create a project without a name' do 
-  	click_button 'Create Project'
+  scenario 'A user cannot create a project without a name' do
+    click_button 'Create Project'
 
-  	expect(page).to have_content("Project has not been created.")
-  	expect(page).to have_content("Name can't be blank")
+    expect(page).to have_content("Project has not been created.")
+    expect(page).to have_content("Name can't be blank")
   end
 end
